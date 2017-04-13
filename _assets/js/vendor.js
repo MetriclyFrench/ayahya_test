d1aac7e6d298bf7ed97dde6c5aa42642c3c35b24
MetriclyFrench <MetriclyFrench@users.noreply.github.com>
2017-04-13 04:18:27 -0700

    Overhauled entire site

---
+++ _assets/js/vendor.js
@@ -0,0 +1,7 @@
+//= require vendor/jquery/jquery-1.12.4.min.js
+//= require plugins/jquery.fitvids.js
+//= require plugins/jquery.greedy-navigation.js
+//= require plugins/jquery.magnific-popup.js
+//= require plugins/jquery.smooth-scroll.min.js
+//= require vendor/anchor.min.js
+//= require vendor/particles.min.js
