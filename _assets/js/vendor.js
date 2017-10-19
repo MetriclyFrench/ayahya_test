f5e7e0aa09384999aad69f28aec2457878e1e009
MetriclyFrench <metriclyfrench@users.noreply.github.com>
2017-10-18 23:20:48 -0700

    Initial Commit

--- _assets/js/vendor.js
+++
@@ -1,7 +0,0 @@
-//= require vendor/jquery/jquery-1.12.4.min.js
-//= require plugins/jquery.fitvids.js
-//= require plugins/jquery.greedy-navigation.js
-//= require plugins/jquery.magnific-popup.js
-//= require plugins/jquery.smooth-scroll.min.js
-//= require vendor/anchor.min.js
-//= require vendor/particles.min.js
